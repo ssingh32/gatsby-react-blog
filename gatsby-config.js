d21686deb3f3b4014ea4f4aa1678db47a0a89665
Sukhdip Singh Nijjar <sukhdipsinghnijjar@dhcp-10-11-202-53.wireless.pacific.edu>
2019-06-20 16:23:57 -0700

    added graphql to pull in some site meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,10 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: "Full-Stack Bootcamp",
+    author: "Sukh Singh"
+  },
   /* Your site config here */
   plugins: [
     "gatsby-plugin-sass"
--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,37 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql, useStaticQuery } from 'gatsby';
 import headerStyles from "./header.module.scss";
 
 const Header = () => {
+    const data = useStaticQuery(graphql` query {
+        site {
+            siteMetadata {
+              title
+            }
+          }
+    }`);
+    
     return (
         <header className={headerStyles.header}>
             <h1>
                 <Link className={headerStyles.title} to="/">
-                    Sukh Singh
+                    {data.site.siteMetadata.title}
                 </Link>
             </h1>
             <nav>
                 <ul className={headerStyles.navList}>
-                    <li><Link className={headerStyles.navItem} to="/">Home</Link></li>
-                    <li><Link className={headerStyles.navItem} to="/about">About</Link></li>
-                    <li><Link className={headerStyles.navItem} to="/blog">Blog</Link></li>
-                    <li><Link className={headerStyles.navItem} to="/contact">Contact</Link></li>
+                    <li>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Blog</Link>
+                    </li>
+                    <li>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contact">Contact</Link>
+                    </li>
                 </ul>
             </nav>
         </header>
