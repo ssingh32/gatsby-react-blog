59b36d3029da502cbba2e444b31a4eafce5ec542
Sukhdip Singh Nijjar <sukhdipsinghnijjar@dhcp-10-11-202-53.wireless.pacific.edu>
2019-06-25 13:44:17 -0700

    pulled data using GraphQL from markdown file

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import Layout from '../components/layout';
+import { graphql, useStaticQuery } from 'gatsby';
 
 const BlogPage = () => {
+    const postData = useStaticQuery(graphql` query {
+        allMarkdownRemark {
+          edges {
+            node {
+              frontmatter {
+                title
+                date
+              }
+            }
+          }
+        }
+      }`);
+      
     return (
         <div>
             <Layout>
                 <h1>Blog Page</h1>
-                <p>Post will show up here later dynamically</p>
+                <ol>
+                    {postData.allMarkdownRemark.edges.map((edge) => {
+                        return (
+                            <li>
+                                <h2>{edge.node.frontmatter.title}</h2>
+                                <p>{edge.node.frontmatter.date}</p>
+                            </li>
+                        )
+                    })}
+                </ol>
             </Layout>
         </div>
     );
